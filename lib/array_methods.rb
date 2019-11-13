fd14fa3560446a1d3d0d11d5733d5f36788f80ff
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-13 01:05:43 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,33 @@
-def using_include(array, element)
+def using_include(array, element) = 
+  ["white", "black", "red"]
+  using_include.include?("red")
+end
+
+def using_sort(array)=
+  ["white", "black", "red"]
+  using_sort.sort!
 
 end
 
-def using_sort(array)
+def using_reverse(array)=
+["white", "black", "red"]
+using_reverse.reverse!
+end
+
+def using_first(array)=
+  ["white", "black", "red"]
+  using_first.first
 
 end
 
-def using_reverse(array)
+def using_last(array)=
+  ["white", "black", "red"]
+  using_last.last
 
 end
 
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
+def using_size(array) = 
+  ["white", "black", "red"]
+  using_size.size
 
 end
